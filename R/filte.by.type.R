578b9e421a8c2e62615e6c7f754724fa478f0863
john collins <johnssocks@gmail.com>
2014-07-22 08:15:29 -0700

    deleted old files

--- R/filte.by.type.R
+++
@@ -1,5 +0,0 @@
-# Filter a dataset's names to only those whose data are of 
-# one of the classes in names.list.
-filter.by.type <- function(dataset, classes.names, names.list) {
-	names(dataset)[unlist(lapply(classes.names,  function(x)length(intersect(x, names.list)) > 0))]
-}
